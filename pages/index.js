9b84fdd7d8a3040c2846b9f95fac880385b6fbc9
Robbie Prokop <robbieprokop@gmail.com>
2022-09-24 14:13:20 +0400

    create article list, article component, article page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,30 @@
 import Head from "next/head"; // used for cutoms titles, meta, etc
+import ArticleList from "../components/ArticleList";
 import Image from "next/image";
 import styles from "../styles/Layout.module.css";
 
-export default function Home() {
+export default function Home({ articles }) {
   return (
     <div>
       <Head>
         <title>WebDev Next</title>
         <meta name="keywords" content="web development, programming, next" />
       </Head>
-      <h1>Welcome to Next</h1>
+
+      <ArticleList articles={articles} />
     </div>
   );
 }
+
+export const getStaticProps = async () => {
+  const res = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=6"
+  );
+  const articles = await res.json();
+
+  return {
+    props: {
+      articles,
+    },
+  };
+};
