0533238589243a92c03ab266e742d4291c1aa089
Robbie Prokop <robbieprokop@gmail.com>
2022-09-24 14:50:22 +0400

    create api

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import { server } from "../config";
 import Head from "next/head"; // used for cutoms titles, meta, etc
 import ArticleList from "../components/ArticleList";
 import Image from "next/image";
@@ -17,9 +18,7 @@ export default function Home({ articles }) {
 }
 
 export const getStaticProps = async () => {
-  const res = await fetch(
-    "https://jsonplaceholder.typicode.com/posts?_limit=6"
-  );
+  const res = await fetch(`${server}/api/articles`);
   const articles = await res.json();
 
   return {
@@ -28,3 +27,15 @@ export const getStaticProps = async () => {
     },
   };
 };
+// export const getStaticProps = async () => {
+//   const res = await fetch(
+//     "https://jsonplaceholder.typicode.com/posts?_limit=6"
+//   );
+//   const articles = await res.json();
+
+//   return {
+//     props: {
+//       articles,
+//     },
+//   };
+// };
